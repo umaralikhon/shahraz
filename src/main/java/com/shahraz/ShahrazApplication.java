c62ad852db25c8dfcfd2ac108d8cbef99035d17a
Umaralikhon <umaralikhon@gmail.com>
2022-06-06 06:41:31 +0500

    Initial commit

---
+++ src/main/java/com/shahraz/ShahrazApplication.java
@@ -0,0 +1,13 @@
+package com.shahraz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShahrazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShahrazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shahraz/ShahrazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahraz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShahrazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
